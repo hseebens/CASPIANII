d94aa6d1f2f66958d865a4b7f74aa26db27daacb
Arne Erpenbach <erpenbach@bafg.de>
2023-07-10 14:46:59 +0200

    (Re-)Upload R scripts for SDM; ermittleUmweltdaten() checks if X11 is available.

---
+++ SDM/R/SDM_decompress_file.R
@@ -0,0 +1,46 @@
+##################################################################################
+# 
+# This script is part of the workflow DASCO to Downscale Alien Species Checklists
+# using Occurrence records from GBIF and OBIS.
+#
+# The DASCO workflow has been published as ..., which has to be cited when used.
+#
+# The function decompresses large zipped csv files efficiently.
+#
+# Authors: Hanno Seebens, Ekin Kaplan, 28.03.2021
+##################################################################################
+
+
+
+decompress_file <- function(directory, file, .file_cache = FALSE) {
+  
+  if (.file_cache == TRUE) {
+    print("decompression skipped")
+  } else {
+    
+    # Set working directory for decompression
+    # simplifies unzip directory location behavior
+    wd <- getwd()
+    setwd(directory)
+    
+    # Run decompression
+    decompression <-
+      system2("unzip",
+              args = c("-o", # include override flag
+                       file),
+              stdout = TRUE)
+    
+    # uncomment to delete archive once decompressed
+    # file.remove(file) 
+    
+    # Reset working directory
+    setwd(wd); rm(wd)
+    
+    # Test for success criteria
+    # change the search depending on 
+    # your implementation
+    if (grepl("Warning message", tail(decompression, 1))) {
+      print(decompression)
+    }
+  }
+} 
