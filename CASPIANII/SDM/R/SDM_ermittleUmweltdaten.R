db695da5dcc2ceb1cdaeb3dda4af515eb5411b5b
hseebens <hanno.seebens@senckenberg.de>
2023-07-11 15:53:22 +0200

    file path interim fix

--- CASPIANII/SDM/R/SDM_ermittleUmweltdaten.R
+++ CASPIANII/SDM/R/SDM_ermittleUmweltdaten.R
@@ -50,7 +50,7 @@ ermittleUmweltdaten <- function(TaxonName=NULL,
   ## land cover data
   if (!is.null(Landbedeck_var)){ # load the raster stack with the land cover data
 
-    LCStack <- stack(paste(file.path("SDM","Data","Input","CorineLandcover",""),c("LC1.tif","LC2.tif", "LC3.tif", "LC4.tif", "LC5.tif", "LC6.tif", "LC7.tif", "LC8.tif", "LC9.tif", "LC10.tif",
+    LCStack <- stack(paste(file.path("SDM","Data","Input","CorineLandcover","/"),c("LC1.tif","LC2.tif", "LC3.tif", "LC4.tif", "LC5.tif", "LC6.tif", "LC7.tif", "LC8.tif", "LC9.tif", "LC10.tif",
                                                                             "LC11.tif","LC12.tif", "LC13.tif", "LC14.tif", "LC15.tif", "LC16.tif", "LC17.tif", "LC18.tif", "LC19.tif", "LC20.tif",
                                                                             "LC21.tif","LC22.tif", "LC23.tif", "LC24.tif", "LC25.tif", "LC26.tif", "LC27.tif", "LC28.tif", "LC29.tif", "LC30.tif",
                                                                             "LC31.tif","LC32.tif", "LC33.tif", "LC34.tif", "LC35.tif", "LC36.tif", "LC37.tif", "LC38.tif", "LC39.tif", "LC40.tif",
