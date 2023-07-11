bdd58b85bb1f473fef485bee10764c318432c63d
Arne Erpenbach <erpenbach@bafg.de>
2023-07-11 18:57:19 +0200

    Use occ_data(taxonKey=...) instead of scientificName; started replacing {raster} calls with {terra} calls; added if-conditions for mf_choro {mapsf} chloropleth map; added comments for understanding and developement --

--- SDM/R/SDM_erstelleKarte_istVorkommenAllerArten.R
+++ SDM/R/SDM_erstelleKarte_istVorkommenAllerArten.R
@@ -39,15 +39,16 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
   values(all_rasters) <- 0
   values(new_raster) <- 0
   
-  n_species <- length(all_files)
+  n_species <- length(all_files) ### note: check value of i (environment) when debugging manually! 
   for (i in 1:n_species){ # loop over occurrence files length(all_files)
-    
-    print(paste(round(i/n_species*100,2),"%"))
+    print(paste("Species",i , "of", n_species,"=", round(i/n_species*100,2),"%"))
     
     ## load occurrences
     Vorkommen <- fread(file=file.path(VorkommenVerzeichnis, all_files[i])) 
     
     ## get taxon name from file name
+    ### note: take care not to conflate internal TaxonName variable with TaxonName variable in environment
+    ### note: could add check if all species in variable artenliste have a corresponding file; may use artenliste[i]? 
     TaxonName <- gsub("Vorkommen_|.csv","",all_files[i])
     TaxonName <- gsub(identifier,"",TaxonName)
     TaxonName <- gsub("_","",TaxonName)
@@ -63,10 +64,10 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
     ## get occurrences per polygon
     pts_regs <- st_join(coords_sf,regions)
     # polys <- pts_regs[!is.na(pts_regs$ID),]
-    polys <- pts_regs$CC_3[!is.na(pts_regs$CC_3)]
+    polys <- pts_regs$CC_3[!is.na(pts_regs$CC_3)] ### note: polys() is an alias for lines{terra}, var should be renamed
     
     # generate rasterized map
-    raster_aliens <- rasterize(all_coords[,c("Laengengrad","Breitengrad")],new_raster)
+    raster_aliens <- rasterize(all_coords[,c("Laengengrad","Breitengrad")],new_raster) ### will convert to {terra} later
     values(raster_aliens)[!is.na(values(raster_aliens))] <- 1
     values(raster_aliens)[is.na(values(raster_aliens))] <- 0
     all_rasters <- all_rasters + raster_aliens
@@ -82,13 +83,14 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
     
     # really_all_coords[[i]] <- all_coords
     
-    all_sites[[i]] <- unique(cbind.data.frame(TaxonName,polys))
-  }
+    all_sites[[i]] <- unique(cbind.data.frame(TaxonName,polys)) ### note: cbind may cause multiple binds when debugging manually.
+  } ## end for-loop over occurence files ### note: depending on the number of files, parallelise later in dev?
   
-  all_sites_df <- do.call("rbind",all_sites)
+  all_sites_df <- do.call("rbind",all_sites) ### note: rbind may cause multiple binds when debugging manually.
+  ### note: all_sites_df <- data.table::rbindlist(all_sites) may be more efficient for large lists.
   # all_coords_df <- do.call("rbind",really_all_coords)
 
-  colnames(all_sites_df) <- c("Taxon","CC_3")
+  colnames(all_sites_df) <- c("Taxon","CC_3") ### note: concatenate may cause multiple binds when debugging manually. 
   
   if (!is.null(Taxa)){
     fwrite(all_sites_df,file.path("SDM","Data","Output", paste0("istVorkommenAlleArten_GADM3_",Taxa,"_",identifier,".gz")))
@@ -108,13 +110,13 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
     ## Plot raster map ################################################################################
     
     Ausschnitt <- (c(3,47,17,55.1)) # crop to Germany  # lower left and upper right corner (long-lat)
-    ext_stack <- extent(c(Ausschnitt[1],Ausschnitt[3],Ausschnitt[2],Ausschnitt[4]))
-    germany  <- crop(all_rasters, ext_stack) # crop the climate data to the extent of the land cover data (needed because the climate data has a global extent and the land cover data has an European extent)
+    # ext_stack <- terra::ext(Ausschnitt) ## not needed, but equivalent result as old code
+    germany  <- terra::crop(all_rasters, Ausschnitt) # crop the climate data to the extent of the land cover data (needed because the climate data has a global extent and the land cover data has an European extent)
     # values(germany)[values(germany)>100] <- 100
     
     # # nSpec_germany <- rasterize(all_coords_df[,c("Laengengrad","Breitengrad")],new_raster)
     # # germany2  <- crop(nSpec_germany, ext_stack) # crop the climate data to the extent of the land cover data (needed because the climate data has a global extent and the land cover data has an European extent)
-    aliens_masked <- mask(germany,germany_border)
+    aliens_masked <- terra::mask(germany,germany_border)
     values(aliens_masked)[values(aliens_masked)>300] <- 300
     
     if (!is.null(Taxa)){
@@ -127,7 +129,7 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
     text(">",x=17.9,y=53.08,xpd=NA)
     dev.off()
 
-    germany2 <- mask(germany,germany_border)
+    germany2 <-  terra::mask(germany,germany_border)
     values(germany2) <- log10(values(germany2)+1)
     
     if (!is.null(Taxa)){
@@ -144,9 +146,8 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
 
     cat("\n Erstelle Karten...\n")
     
-    all_sites_agg <- aggregate(Taxon ~ CC_3, data=all_sites_df,length)
-    
-    regions <- merge(regions,all_sites_agg,by="CC_3")
+    all_sites_agg <- aggregate(Taxon ~ CC_3, data=all_sites_df,length) ### note:  aggregate::stats() could be masked if {terra} or {sf} are loaded.
+    regions <- terra::merge(regions,all_sites_agg,by="CC_3") ### note: merge may cause multiple binds when debugging manually.
     regions$Taxon[regions$Taxon>300] <- 300
     
     if (!is.null(Taxa)){
@@ -154,14 +155,15 @@ erstelleKarte_istVorkommenAlle <- function(VorkommenVerzeichnis=VorkommenVerzeic
     } else {
       png(file.path("SDM","Data","Output","Grafiken",paste0("KarteDeutschland_VorkommenAlle_GADM3_max100_",identifier,".png")),unit="in",width=8,height=8,res=300)
     }
-    mf_choro(regions,var="Taxon",leg_title="Anzahl Neobiota",border=NA,breaks="pretty") #,breaks="pretty"
-    text(">",x=17.85,y=53,xpd=NA)
+    if(is.numeric(regions$Taxon) && length(unique(regions$Taxon)) > 1){
+    mf_choro(regions,var="Taxon",leg_title="Anzahl Neobiota",border=NA,breaks="pretty") #,breaks="pretty" ### note: this does not break pretty. ^__^ 
+    text(">",x=17.85,y=53,xpd=NA)}
     dev.off()
 
-  }
+  } ## end if(exportiereKarte)
+
 
 }
-
 
 
 
